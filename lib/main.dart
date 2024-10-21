231e48056ecbb7f13ad2ec40dd8c46852fcb2159
DivakarReddyGade <22a91a05h5@aec.edu.in>
2024-10-21 12:08:54 +0530

    divcar

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:uchef/screens/signup.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,1 +1,64 @@
-//sign up divakar
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                  height: 300,
+                  width: 400,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40)
+                  ),
+                  child: Stack(
+                    children: [
+                      // Center(
+                      //   child: Text('Sign UP'),
+                      // ),
+                      Positioned(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white.withOpacity(0.50)
+                            ),
+                            child: TextFormField(
+                              clipBehavior: Clip.none,
+                              decoration: InputDecoration(
+                                  hintText: 'Enter Your Mail',
+                                  labelText: 'Username'
+                              ),
+                            ),
+                          ))
+                    ],
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
