342034b9b6df15d9d56176d72bbe8505ff362c08
kranthivarm <22A91A05G2@aec.edu.in>
2024-10-23 10:50:37 +0530

    kranthi

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+//Gemini API key
+// 
+// AIzaSyCcij1CWqvBasRXxM2axFVXNOfMFvvkios
+// 
+
+
+import 'firebase_options.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MyApp());
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(      
+      debugShowCheckedModeBanner: false,
+      // home: AuthChange(),
+      // home:FireStore()
+      
     );
   }
 }
